f363f844da56f16edd8fb346da5c729aadb97e8f
Mayur Vaity <mayur.m.vaity@icloud.com>
2024-07-30 22:15:23 +0530

    Initial Commit

---
+++ FaceFacts/FaceFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceFactsApp.swift
+//  FaceFacts
+//
+//  Created by Mayur Vaity on 30/07/24.
+//
+
+import SwiftUI
+
+@main
+struct FaceFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
